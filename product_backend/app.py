a5eeb0b484dbd7497d21cc633fa259ac1da065b6
Siri Gowri H <106529095+DarkKnightSgh@users.noreply.github.com>
2024-04-20 19:49:42 +0530

    Update app.py

--- product_backend/app.py
+++ product_backend/app.py
@@ -64,8 +64,7 @@ def add_product():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
