ad68bee579676e6972fa826d95f5312eb8176299
Siri Gowri H <106529095+DarkKnightSgh@users.noreply.github.com>
2024-04-20 19:53:13 +0530

    Update paths home.js

--- adroit-18/src/pages/home.js
+++ adroit-18/src/pages/home.js
@@ -14,7 +14,7 @@ const Home = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/products");
+      const response = await fetch("http://127.0.0.1:8080/api/products");
       if (!response.ok) {
         throw new Error("Failed to fetch products");
       }
